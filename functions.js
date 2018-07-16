9dc6d623e681c431cb854e3206365a3819756032
Timo <lehttim@gmail.com>
2018-07-17 00:45:39 +0300

    Created function to check hand

--- functions.js
+++ functions.js
@@ -1,32 +1,60 @@
 window.onload = function () {
-  let hand = [];
+  let shuffle = [];
   let table = document.getElementById('table');
   let deck = document.getElementById('deck');
 
   deck.onclick = () => {
-    hand.length = 0;
+    shuffle.length = 0;
     for (let i = 0; i < 5; i++) {
       let card = {
         suit : Math.floor((Math.random() * 4) + 1),
         num : Math.floor((Math.random() * 13) + 1)
       }
-      hand.push(card);
+      shuffle.push(card);
     }
-    table.innerHTML = JSON.stringify(hand);
+    table.innerHTML = JSON.stringify(shuffle);
     check();
   }
 
   function check() {
-    let suits = hand.map(x => x.suit);
-    let flush = suits.every(
-      (value, _, array) => {
-        return array[0] === value;
+    let flush, straight, hand = "";
+    let suits = shuffle.map(x => x.suit);
+    let nums = shuffle.map(x => x.num).sort((a,b) => {return a - b});
+
+    function checkFlush(value, i, array) {
+      return array[0] === value;
+    }
+    flush = suits.every(checkFlush);
+
+    function checkStraight(value, i, array) {
+      if (array[i] == array[i + 1] - 1 || array[i + 1] == null) {
+        return true;
+      } else {
+        return false;
       }
-    );
-    if (flush == true) {
-      console.log('It is a Flush!');
-    } else {
-      console.log('You win nothing.');
+    }
+    straight = nums.every(checkStraight);
+
+    if (flush === true && straight === false) {
+      hand = "flush";
+    } else if (flush === false && straight === true) {
+      hand = "straight";
+    } else if (flush === true && straight === true) {
+      hand = "straight flush";
+    }
+
+    switch (hand) {
+      case "flush":
+        console.log("It's a flush!");
+        break;
+      case "straight":
+        console.log("It's a straight!");
+        break;
+      case "straight flush":
+        console.log("It's a straight flush!");
+        break;
+      default:
+        console.log('You have nothing.');
     }
   }
 };
